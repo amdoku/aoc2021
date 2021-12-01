2445514affc6219585c01287cb08b59cc147c607
amdoku <aqpoba@gmail.com>
2021-12-01 19:11:04 +0100

    Finish Day01 - 2
    A very ugly solution, but it has been a long week and I can't be
    bothered.

--- Day01/main.cpp
+++ Day01/main.cpp
@@ -1,7 +1,9 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <numeric>
 #include <vector>
+
 #include <utilities.hpp>
 
 int main(int argc, char ** argv) {
@@ -19,6 +21,7 @@ int main(int argc, char ** argv) {
 
 	std::vector<int64_t> const input{start, end};
 
+	// 01-1
 	ssize_t incrementCount = std::count_if(++std::begin(input), std::end(input), [prevVal = *std::begin(input)] (int64_t currVal) mutable -> bool {
 			bool reti = currVal > prevVal;
 			prevVal = currVal;
@@ -26,7 +29,22 @@ int main(int argc, char ** argv) {
 	});
 
 	std::cout << "1 - solution: " << incrementCount << '\n';
-	
+
+	// 01-2
+	// Ugly AF, bc it is late and I am tired.
+	std::vector<int64_t> sums{};
+	auto out {std::back_inserter(sums)};
+	for(auto idx{0u}; idx < input.size() - 2u; idx++) {
+		out = std::accumulate(std::next(std::begin(input), idx), std::next(std::begin(input), idx + 3u), 0l);
+		out++;
+	}
+	ssize_t incrementWindowCount = std::count_if(++std::begin(sums), std::end(sums), [prevVal = *std::begin(sums)] (int64_t currVal) mutable -> bool {
+		bool reti = currVal > prevVal;
+		prevVal = currVal;
+		return reti;
+	});
+
+	std::cout << "2 - solution: " << incrementWindowCount << '\n';
 	return 0;
 }
 
